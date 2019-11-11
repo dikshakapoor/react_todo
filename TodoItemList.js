3cfaeddb1ba36d1acae918656a47cc6b618f500f
dikshakapoor <52195441+dikshakapoor@users.noreply.github.com>
2019-11-11 10:29:28 +0530

    few changes

--- TodoItemList.js
+++ TodoItemList.js
@@ -3,62 +3,152 @@ import React from "react";
 import completeButtonImg from "./_ionicons_svg_md-checkmark-circle.svg";
 import removeButtonImg from "./_ionicons_svg_md-trash.svg";
 import editButtonImg from "./_ionicons_svg_md-create.svg";
+
 class TodoItemList extends React.PureComponent {
-    createTasks(items) {
-        return <li key={items.key}> {items.text} </li>
-    }
-    // state = {
-    //     todos: [
-    //         {
-    //             text: null,
-    //             id: Date.now(),
-    //             status: "",
-    //         }
-    //     ]
-    // }
     // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         text = null,
-    //         id = Date.now(),
-    //         status = ""
+    //     super(props)
+    // }
+    // class TodoItemList extends React.PureComponent {
+    //     constructor(props) {
+    //         super(props);
+    //         this.state =  
+    //     }
+    //     render() {
+    //         console.log(props);
+    //         console.log(this.props.state);
+    //         return null;
+    //     }
+
+
+    // }
+
+    handleMarkComplete = (event) => {
+        console.log(event);
+        const { managerSetState } = this.props;
+        debugger
+        managerSetState({
+            arr: [1, 4]
+        })
+    }
+    handleAddStatus = (event) => {
+        console.log(event);
+        //status = event.currentTarget.__reactEventHandlers$6bb48irirc3.className;
+        const { addStatus } = this.props;
+
+        // props.addStatus("removed", task.id)
+        // this.props.addStatus("edited", task.id)
+
+    };
+
+    // const setStatus = (id) => {
+
+    //     props.entries.taskList.map((task) => {
+    //         switch (task.status) {
+    //             case "edited": this.editTask(task.id);
+    //                 break;
+    //             case "completed": this.compelteTask(task.id);// <div className += "checked"/>;
+    //                 break;
+    //             case "removed": this.removeTask(task.id);
+    //                 break;
+    //         }
 
     //     }
-    //     this.setState({
 
-    //     })
+    //     )
+    // };
 
-    // }
+    // console.log(props.entries.taskList);
+    // console.log("this is todolist");
+    // console.log(props.taskList);
     render() {
-        let todoEntries = this.props.entries;
-        let listItems = todoEntries.map(this.createTasks);
-        return (<div className="card">
-            <div className="task"><b>{listItems}</b></div>
-            <div className="icon">
-                <button className="completed">
-                    <img src={completeButtonImg} alt="complete" style={{ width: "20px", heigth: "20px" }} /></button>
-                <button className="removed">
-                    <img src={removeButtonImg} alt="delete" style={{ width: "20px", height: "20px" }} /></button>
-                <button className="edited">
-                    <img src={editButtonImg} alt="edit" style={{ width: "20px", heigth: "20px" }} /></button>
-            </div>
-        </div>
+        if (this.props.entries.taskList) {
 
-        );
 
-        // const todoList = todos.length ? (todos.map(todo => {
-        //     return (
+            return (
+                <div>
 
-        //     )
-        // })) : (null);
+                    {this.props.entries.taskList.map((task) => {
 
-        // const { todos } = this.state;
-        // debugger;
-        // return (
+                        return (<div className="card">
+                            <div className="task" key={task.key} ><b>{task.value}</b></div>
+                            <div className="icon" >
 
-        // )
+                                <button className="completed" onClick={this.handleMarkComplete}>
+                                    <img src={completeButtonImg} alt="complete" style={{ width: "20px", heigth: "20px" }} /></button>
+                                <button className="removed" onClick={this.handleDeleteItem} >
+                                    <img src={removeButtonImg} alt="delete" style={{ width: "20px", height: "20px" }} /></button>
+                                <button className="edited" onClick={this.handleEditItem}>
+                                    <img src={editButtonImg} alt="edit" style={{ width: "20px", heigth: "20px" }} /></button>
+                            </div>
+                        </div>
+                        )
+
+
+                    })
+                    }
+                </div>
+            );
+        }
     }
-
 }
+
+
+// class TodoItemList extends React.PureComponent {
+//     createTasks(items) {
+//         return <li key={items.key}> {items.text} </li>
+//     }
+//     // state = {
+//     //     todos: [
+//     //         {
+//     //             text: null,
+//     //             id: Date.now(),
+//     //             status: "",
+//     //         }
+//     //     ]
+//     // }
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //         text = null,
+//     //         id = Date.now(),
+//     //         status = ""
+
+//     //     }
+//     //     this.setState({
+
+//     //     })
+
+//     // }
+//     render() {
+//         let todoEntries = this.props.entries;
+//         let listItems = todoEntries.map(this.createTasks);
+//         return (<div className="card">
+//             <div className="task"><b>{listItems}</b></div>
+//             <div className="icon">
+//                 <button className="completed">
+//                     <img src={completeButtonImg} alt="complete" style={{ width: "20px", heigth: "20px" }} /></button>
+//                 <button className="removed">
+//                     <img src={removeButtonImg} alt="delete" style={{ width: "20px", height: "20px" }} /></button>
+//                 <button className="edited">
+//                     <img src={editButtonImg} alt="edit" style={{ width: "20px", heigth: "20px" }} /></button>
+//             </div>
+//         </div>
+
+//         );
+
+//         // const todoList = todos.length ? (todos.map(todo => {
+//         //     return (
+
+//         //     )
+//         // })) : (null);
+
+//         // const { todos } = this.state;
+//         // debugger;
+//         // return (
+
+//         // )
+//  }
+
+// }
 
 export default TodoItemList;
--- TodoManager.js
+++ TodoManager.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './TodoManager.css';
 import NavigationBar from "./NavigationBar";
 // import InputForm from "./InputForm";
-// import TodoItemList from "./TodoItemList";
+import TodoItemList from "./TodoItemList";
 
-class TodoManager extends React.Component {
+class TodoManager extends React.PureComponent {
   constructor(props) {
-    super(props)
+    super(props) // props in case we pass something from other class
     // this.addItem = this.addItem.bind(this);
     // let taskMap = new Map
     this.state = {
       taskList: [],
-      currentInputValue: ""
+      currentInputValue: "",
+      status: ""
     }
+
+    this.setState = this.setState.bind(this);
   }
 
   // addItem(e) {
@@ -51,33 +54,50 @@ class TodoManager extends React.Component {
   createNewTask = () => {
     const { taskList } = this.state;
     console.log(taskList);
+    console.log(typeof (taskList));
   }
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
+  onInputSubmit = (event) => {
+
     const { taskList, currentInputValue } = this.state; // destructure
-    const newTask = {
-      key: Date.now(),
-      value: currentInputValue,
-    };
-    this.setState({
-      currentInputValue: '',
-      taskList: [...taskList, newTask],
-    }, this.createNewTask)
-    console.log(this.state.currentInputValue);
-  }
-  keyPressHandle = (event) => { // not working 
+    if (currentInputValue !== " ") {
+      const newTask = {
+        key: Date.now(),
+        value: currentInputValue,
+        status: ""
 
-    if (event.which === 13) {
-      this.handleInputChange();
+      };
+      this.setState({
+        currentInputValue: '',
+        taskList: [...taskList, newTask],
 
-      console.log("working");
+      }, this.createNewTask)
+      console.log(this.state.currentInputValue);
     }
   }
+  keyPressHandle = (event) => {
 
+    if (event.which === 13) {
+      this.setState({
+        currentInputValue: event.currentTarget.value,
+      })
+      this.onInputSubmit();
+
+    }
+  }
+  addStatus = (taskStatus, id) => {
+    this.state.taskList.map((task) => {
+      if ((task.id) === id) {
+
+        task.status = taskStatus;
+      }
+    })
+
+  }
 
   handleInputChange = (ev) => {
     console.log(ev.target.value);
+
     this.setState({
 
       currentInputValue: ev.target.value,
@@ -85,35 +105,39 @@ class TodoManager extends React.Component {
   }
 
   render() {
-    const { currentInputValue } = this.state;
+    const { currentInputValue, taskList } = this.state;
     return (
-      <div>
-        <div className="container">
-          <NavigationBar />
-          {/* <form onSubmit="return false"> */}
-          {/* <form onSubmit={this.onFormSubmit}> */}
-          <h2>TODO LIST</h2>
-          <div className="input-container">
-            <input
-              className="inputfield" type="text"
-              value={currentInputValue}
-              placeholder="Type here.."
-              onKeyPress={this.keyPressHandle}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div className="wrapper">
-            <button onClick={this.onFormSubmit} className="addTaskButton"><strong>ADD </strong></button>
-          </div>
-          {/* </form> */}
-          <ul className="taskList_wrapper">
-            {/* <TodoItemList entries={this.state} /> */}
-
-          </ul>
+      <div className="container">
+        <NavigationBar />
+        {/* <form onSubmit="return false"> */}
+        {/* <form onSubmit={this.onFormSubmit}> */}
+        <h2>TODO LIST</h2>
+        <div className="input-container">
+          <input
+            className="inputfield" type="text"
+            value={currentInputValue}
+            placeholder="Type here.."
+            onKeyPress={this.keyPressHandle}
+            onChange={this.handleInputChange}
+          />
         </div>
+        <div className="wrapper">
+          <button onClick={this.onInputSubmit} className="addTaskButton">
+            <strong>ADD </strong>
+          </button>
+        </div>
+        {/* </form> */}
+        <ul className="taskList_wrapper">
+          <TodoItemList entries={taskList} addStatus={this.addStatus} managerSetState={this.setState} />
+          {/* <operationOnTask entries={this.state.status} /> */}
+        </ul>
       </div>
     );
   }
 }
 
 export default TodoManager;
+
+
+// trim the input 
+// add the event handler for edit delete and compelte
